53be39a82ff89b665020ea72ff2b14b64790e9ea
Joseph Mathew <jojo.math@gmail.com>
2020-03-28 13:40:43 -0600

    dir() explained with example

---
+++ Notes/notes.py
@@ -0,0 +1,9 @@
+# Lists all the build in functions and attributes available
+builtIns = dir(__builtins__)
+print("====== Built Ins =====")
+print(builtIns)
+
+# if you want to know methods or attributes in a particular container try dir(), example below
+listMethods = dir(list)
+print("====== List =====")
+print(listMethods)
